c245ec45bbf16bc629eaa396a36f1685cffcc684
Idam <idamachmadfaizin@gmail.com>
2020-08-11 01:26:27 +0700

    register .env in index

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const app = express();
+const dotenv = require('dotenv');
 const mongoose = require('mongoose');
+// Environment
+dotenv.config();
+
 // Connect to DB
 mongoose.connect(
     process.env.DB_CONNECTION,
