4d26e61778696a1c69cf34f38b875ead82e476fe
Idam <idamachmadfaizin@gmail.com>
2020-08-11 01:29:37 +0700

    register auth route in index and give prefix

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ const express = require('express');
 const app = express();
 const dotenv = require('dotenv');
 const mongoose = require('mongoose');
+
+// Import Routes
+const authRoute = require('./routes/auth-route');
+
 // Environment
 dotenv.config();
 
@@ -11,4 +15,10 @@ mongoose.connect(
     { useNewUrlParser: true },
     () => console.log('Successfully connect to MongoDb')
 );
+
+//Middleware
+app.use(express.json());
+// Route Middleware
+app.use('/api/user', authRoute);
+
 app.listen(process.env.APP_PORT, () => console.log('Server is successfully running'));
